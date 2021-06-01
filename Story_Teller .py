87635711edf874d07a64c3f27fbf5e755f06053f
Rati Priya <82707008+Rati30@users.noreply.github.com>
2021-06-01 14:19:52 +0530

    Add files via upload

---
+++ Story_Teller .py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+when = ['A few year ago','Yesterady','Last night','Long time ago','On 20th May']
+who = ['a rabbit','an elephant','a mouse','a tiger','a cat']
+name= ['Danniel','Swamy','Sonu','Rikki','Chikki']
+address = ['India','Barcelona','Australia','Maxico','Wales']
+went = ['cinema','home','collage','lab','hospital']
+happened = ['ate cookies','slept','drank juice','runned','played football']
+print(random.choice(when) + ' '+ random.choice(who) + " "+ 'that lived in'+ " "+ random.choice(address) + " " 
+      + 'his name was'+ " " + random.choice(name) + " " + 'he went to'+ " " + random.choice(went) + " " + 'there he' + " " +random.choice(happened))
+
+
+# In[ ]:
+
+
+
+
